5420674f5e4462fc8b1a6c3917cfb87ec537c11c
dhruv50ae <linkdp2012@gmail.com>
2022-04-30 23:28:32 +0100

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Route, BrowserRouter } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/streams/new" component={StreamCreate} />
+          <Route exact path="/streams/edit" component={StreamEdit} />
+          <Route exact path="/streams/delete" component={StreamDelete} />
+          <Route exact path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
