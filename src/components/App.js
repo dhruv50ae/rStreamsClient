32c8563d936d6329911853aa9d62028e681a3983
dhruv50ae <linkdp2012@gmail.com>
2022-04-30 19:07:19 +0100

    Added Links

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Route, BrowserRouter } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
 };
 const PageTwo = () => {
-  return <div>PageTwo</div>;
+  return (
+    <div>
+      PageTwo
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
 };
 
 const App = () => {
