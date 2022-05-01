06604bbcb00090035bfee677ff5964ef9c22c3d1
dhruv50ae <linkdp2012@gmail.com>
2022-05-01 20:29:52 +0100

    Fixed bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
-import Header from "./Header";
-import StreamCreate from "./streams/StreamCreate";
-import StreamDelete from "./streams/StreamDelete";
-import StreamEdit from "./streams/StreamEdit";
-import StreamList from "./streams/StreamList";
-import StreamShow from "./streams/StreamShow";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
@@ -13,11 +13,11 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Header />
-          <Route exact path="/" component={StreamList} />
-          <Route exact path="/streams/new" component={StreamCreate} />
-          <Route exact path="/streams/edit" component={StreamEdit} />
-          <Route exact path="/streams/delete" component={StreamDelete} />
-          <Route exact path="/streams/show" component={StreamShow} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,58 +1,71 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { signIn, signOut } from "../actions";
-class GoogleAuth extends Component {
-  state = { isSignedIn: null };
+import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends React.Component {
   componentDidMount() {
-    window.gapi.load("client: auth2", () => {
+    window.gapi.load('client:auth2', () => {
       window.gapi.client
         .init({
           clientId:
-            "963151505353-0745rrpgg77hlak1b74usm5i5h1t75ud.apps.googleusercontent.com",
-          scope: "email",
+            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+          scope: 'email'
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
-  onAuthChange = (isSignedIn) => {
+
+  onAuthChange = isSignedIn => {
     if (isSignedIn) {
       this.props.signIn();
     } else {
       this.props.signOut();
     }
   };
+
   onSignInClick = () => {
     this.auth.signIn();
   };
+
   onSignOutClick = () => {
     this.auth.signOut();
   };
+
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
-          Sign out
+          Sign Out
         </button>
       );
     } else {
       return (
         <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon" />
-          Sign in with Google
+          Sign In with Google
         </button>
       );
     }
   }
+
   render() {
     return <div>{this.renderAuthButton()}</div>;
   }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(
+  mapStateToProps,
+  { signIn, signOut }
+)(GoogleAuth);
