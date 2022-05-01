979eb839f06118664ed2a7d211c2f11658b82d13
dhruv50ae <linkdp2012@gmail.com>
2022-05-01 14:24:19 +0100

    Major bug fixes and complete implementation

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -20,19 +20,25 @@ export default class GoogleAuth extends Component {
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+  onSignOut = () => {
+    this.auth.signOut();
+  };
   renderAuthButton() {
-    if (this.state.isSignedIn) {
+    if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignOut} className="ui red google button">
           <i className="google icon" />
           Sign out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignIn} className="ui red google button">
           <i className="google icon" />
           Sign in with Google
         </button>
