922f6799ced309e9c5dd585e5b12e5c8b41857b4
dhruv50ae <linkdp2012@gmail.com>
2022-05-01 19:14:48 +0100

    Created redux foundation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import reducer from "./reducers";
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
