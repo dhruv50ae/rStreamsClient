c898bfa72828d21c9a20517ed051ba5963a57598
dhruv50ae <linkdp2012@gmail.com>
2022-05-03 15:48:43 +0100

    Finished validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -12,8 +12,9 @@ class StreamCreate extends Component {
     }
   };
   renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.touched && meta.error ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
         {this.renderError(meta)}
@@ -27,7 +28,7 @@ class StreamCreate extends Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter title" />
         <Field
